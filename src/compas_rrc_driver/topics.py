7e61c24fe05d01238a60305c0dc1298e74c3cd8a
Mattis Koh <mattiskoh@gmail.com>
2026-02-11 10:14:18 +0100

    Migrate driver package to ROS 2 stack

--- src/compas_rrc_driver/topics.py
+++ src/compas_rrc_driver/topics.py
@@ -1,6 +1,7 @@
-import rospy
 from threading import Lock
-from compas_rrc_driver import msg
+
+from compas_rrc_driver.msg import RobotMessage
+
 
 class SequenceCheckModes(object):
     NONE = 'none'
@@ -10,9 +11,10 @@ class SequenceCheckModes(object):
 
 
 class RobotMessageTopicProvider(object):
-    def __init__(self, topic_name_sub, topic_name_pub, streaming_interface, robot_state, options=None):
+    def __init__(self, node, topic_name_sub, topic_name_pub, streaming_interface, robot_state, options=None):
         super(RobotMessageTopicProvider, self).__init__()
 
+        self.node = node
         self._publish_lock = Lock()
         self._receive_lock = Lock()
         self._last_published_id = 0
@@ -22,20 +24,24 @@ class RobotMessageTopicProvider(object):
         if options:
             self.sequence_check_mode = options.get('sequence_check_mode', SequenceCheckModes.NONE)
 
-            if self.sequence_check_mode not in (SequenceCheckModes.NONE, SequenceCheckModes.ALL, SequenceCheckModes.INCOMING, SequenceCheckModes.OUTGOING):
+            if self.sequence_check_mode not in (
+                SequenceCheckModes.NONE,
+                SequenceCheckModes.ALL,
+                SequenceCheckModes.INCOMING,
+                SequenceCheckModes.OUTGOING,
+            ):
                 raise Exception('Unsupported sequence check mode. Value={}'.format(self.sequence_check_mode))
 
         self.streaming_interface = streaming_interface
         self.robot_state = robot_state
 
-        # TODO: Verify topic queue sizes
-        self.subscriber = rospy.Subscriber(topic_name_sub, msg.RobotMessage, self.ros_to_robot_handler)
-        self.publisher = rospy.Publisher(topic_name_pub, msg.RobotMessage, queue_size=1)
+        self.subscriber = self.node.create_subscription(RobotMessage, topic_name_sub, self.ros_to_robot_handler, 10)
+        self.publisher = self.node.create_publisher(RobotMessage, topic_name_pub, 10)
 
         self.robot_state.on_message(self.robot_to_ros_handler)
         self.robot_state.on_socket_broken(self._reset_sequence_id)
 
-        rospy.loginfo('Topic provider started. Subscribed to %s, publishing to %s', topic_name_sub, topic_name_pub)
+        self.node.get_logger().info('Topic provider started. Subscribed to %s, publishing to %s' % (topic_name_sub, topic_name_pub))
 
     def _reset_sequence_id(self):
         with self._publish_lock:
@@ -45,15 +51,20 @@ class RobotMessageTopicProvider(object):
             self._last_received_id = 0
 
     def ros_to_robot_handler(self, ros_message):
-        """Handle messages from ROS topics to the robot controller"""
+        """Handle messages from ROS topics to the robot controller."""
         try:
             with self._publish_lock:
                 if self.sequence_check_mode in (SequenceCheckModes.OUTGOING, SequenceCheckModes.ALL):
                     if ros_message.sequence_id != self._last_published_id + 1:
-                        raise Exception('Received out of order (ROS -> Controller). Received={}, Last sequence id={}'.format(ros_message.sequence_id, self._last_published_id))
+                        raise Exception(
+                            'Received out of order (ROS -> Controller). Received={}, Last sequence id={}'.format(
+                                ros_message.sequence_id,
+                                self._last_published_id,
+                            )
+                        )
                 self.streaming_interface.execute_instruction(ros_message)
         except Exception as e:
-            rospy.logerr(e)
+            self.node.get_logger().error(str(e))
             raise e
         finally:
             self._last_published_id = ros_message.sequence_id
@@ -64,18 +75,23 @@ class RobotMessageTopicProvider(object):
             try:
                 if self.sequence_check_mode in (SequenceCheckModes.INCOMING, SequenceCheckModes.ALL):
                     if ros_message.sequence_id != self._last_received_id + 1:
-                        raise Exception('Received out of order (Controller -> ROS). Received={}, Last sequence id={}'.format(ros_message.sequence_id, self._last_received_id))
+                        raise Exception(
+                            'Received out of order (Controller -> ROS). Received={}, Last sequence id={}'.format(
+                                ros_message.sequence_id,
+                                self._last_received_id,
+                            )
+                        )
                 self.publisher.publish(ros_message)
             except Exception as e:
-                rospy.logerr(e)
+                self.node.get_logger().error(str(e))
                 raise e
             finally:
                 self._last_received_id = ros_message.sequence_id
 
     def disconnect(self):
         try:
-            self.subscriber.unregister()
-            self.publisher.unregister()
-            rospy.loginfo('Topic provider disconnected')
+            self.node.destroy_subscription(self.subscriber)
+            self.node.destroy_publisher(self.publisher)
+            self.node.get_logger().info('Topic provider disconnected')
         except Exception as e:
-            rospy.logerr(e)
+            self.node.get_logger().error(str(e))
